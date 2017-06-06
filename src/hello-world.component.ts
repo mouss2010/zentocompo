8f52c900aaf68753f8e72daf2a2b4185c22cf81c
almostafa.nassih@zento.fr <Xq@9863naz>
2017-06-06 09:56:35 +0200

    test: change component

--- src/hello-world.component.ts
+++ src/hello-world.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'zto-hello-world',
-  template: 'Hello world from the {{ projectTitle }} module!'
+  template: 'Mouss world from the {{ projectTitle }} module!'
 })
 export class HelloWorldComponent {
   projectTitle: string = 'zentocompo';
